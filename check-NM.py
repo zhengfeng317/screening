2ad0aa07e5988d43923171068938aceb642168c7
Yang Sun <ys3339@br012.ib.bridges2.psc.edu>
2022-04-13 00:46:11 -0400

    check NM calculation

---
+++ check-NM.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import glob
+
+
+def get_bad(files):
+    lst=[] 
+    for ifl in files:
+        fin=open(ifl,"r")
+        lines=fin.readlines()
+        fin.close()
+        if(len(lines)==0):
+            print(ifl,"no data")
+            lst.append(sys)
+        elif("reached required" not in lines[-1]):
+            sys=ifl.split("/")[1]
+            print(ifl,'not converged')
+            lst.append(sys)
+        else:
+            print(ifl,'done')
+    return lst
+
+files=glob.glob("NM/*/vasp.3")
+lst_NM=get_bad(files)
